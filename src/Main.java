071b32055bfaa35eccaa038db60b5e2593c36da3
mohamedali69 <dalichakhari6801@gmail.com>
2023-11-26 17:27:40 +0100

    Add User Class

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,7 @@ public class Main {
                 System.out.println("Thank you for using the Organisation Management System!");
                 break;
             default:
-                System.out.println("Invalid option Bro!");
+                System.out.println("Invalid option!");
                 break;
         }
     }
