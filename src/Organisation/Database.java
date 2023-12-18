c917a96698f2c0a7e766d03e90b02636a765dd56
mohamedali69 <dalichakhari6801@gmail.com>
2023-12-18 20:13:09 +0100

    add user file creation

--- src/Organisation/Database.java
+++ src/Organisation/Database.java
@@ -1,18 +1,34 @@
 package Organisation;
+
+import com.sun.tools.javac.Main;
+
+import java.awt.print.Book;
+import java.io.File;
 import java.util.ArrayList;
 public class Database {
-    ArrayList<User> users = new ArrayList<User>();
-    ArrayList<String> userNames = new ArrayList<String>();
-    ArrayList<String> userEmails = new ArrayList<String>();
-    ArrayList<String> userPasswords = new ArrayList<String>();
-    ArrayList<String> userRoles = new ArrayList<String>();
+    private ArrayList<User> users = new ArrayList<User>();
+    private ArrayList<String> userNames = new ArrayList<String>();
+    private ArrayList<Event> events  = new ArrayList<Event>();
+    private ArrayList<String> eventNames = new ArrayList<String>();
 
+    private File usersfile = new File("C:\\Users\\moham\\IdeaProjects\\UnityGives\\Data\\Users");
+    private File folder = new File("C:\\Users\\moham\\IdeaProjects\\UnityGives\\Data");
+
+    public Database() {
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+        if (!usersfile.exists()) {
+            try {
+                usersfile.createNewFile();
+            } catch (Exception e) {}
+        }
+
+        users = new ArrayList<User>();
+    }
     public void addUser(User user) {
         users.add(user);
         userNames.add(user.getName());
-        userEmails.add(user.getEmail());
-        userPasswords.add(user.getPassword());
-        userRoles.add(user.getRole());
     }
     public int login(String email, String password) {
         int found = -1;
@@ -27,4 +43,11 @@ public class Database {
     public User getUser(int found) {
         return users.get(found);
     }
+
+    public void AddEvent(Event event){
+        events.add(event);
+        eventNames.add(event.getName());
+    }
+
+
 }
--- src/Organisation/Event.java
+++ src/Organisation/Event.java
@@ -1,4 +1,65 @@
 package Organisation;
 
 public class Event {
+    private String name;
+    private String type;
+    private String eventDate;
+    private String location;
+    private String description;
+    public Event(){}
+    public Event(String name, String type, String eventDate, String location, String description){
+        this.name = name;
+        this.type = type;
+        this.eventDate = eventDate;
+        this.location = location;
+        this.description=description;
+    }
+
+    public String toString() {
+        String text = "Event Name :" + name +
+                "Event Type :" + type +
+                "Event Date :" + eventDate +
+                "Event location :" + location +
+                "Event Description :" + description;
+        return  text;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
