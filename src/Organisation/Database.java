a41a9f0e6f717bb67905575a96585e44dcabd761
mohamedali69 <dalichakhari6801@gmail.com>
2023-12-18 21:02:49 +0100

    add postgre db connection

--- src/Organisation/Database.java
+++ src/Organisation/Database.java
@@ -1,53 +1,67 @@
 package Organisation;
 
-import com.sun.tools.javac.Main;
+import java.sql.*;
 
-import java.awt.print.Book;
-import java.io.File;
-import java.util.ArrayList;
 public class Database {
-    private ArrayList<User> users = new ArrayList<User>();
-    private ArrayList<String> userNames = new ArrayList<String>();
-    private ArrayList<Event> events  = new ArrayList<Event>();
-    private ArrayList<String> eventNames = new ArrayList<String>();
-
-    private File usersfile = new File("C:\\Users\\moham\\IdeaProjects\\UnityGives\\Data\\Users");
-    private File folder = new File("C:\\Users\\moham\\IdeaProjects\\UnityGives\\Data");
+    private Connection connection;
 
     public Database() {
-        if (!folder.exists()) {
-            folder.mkdirs();
+        // Establish connection to NeonDB
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/unitygives_db", "postgres", "Dali123;");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
-        if (!usersfile.exists()) {
-            try {
-                usersfile.createNewFile();
-            } catch (Exception e) {}
-        }
-
-        users = new ArrayList<User>();
     }
-    public void addUser(User user) {
-        users.add(user);
-        userNames.add(user.getName());
+    public boolean addUser(User user) {
+        String SQL = "INSERT INTO users(name, email, password, role) VALUES(?,?,?,?)";
+
+        try (PreparedStatement pstmt = connection.prepareStatement(SQL)) {
+            pstmt.setString(1, user.getName());
+            pstmt.setString(2, user.getEmail());
+            pstmt.setString(3, user.getPassword());
+            pstmt.setString(4, user.getRole());
+            int affectedRows = pstmt.executeUpdate();
+            return affectedRows > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
     public int login(String email, String password) {
-        int found = -1;
-        for (User s : users) {
-            if (s.getEmail().matches(email) && s.getPassword().matches(password)) {
-                found= users.indexOf(s);
-                break;
+        String SQL = "SELECT * FROM users WHERE email = ? and password = ?";
+        try (PreparedStatement pstmt = connection.prepareStatement(SQL)) {
+            pstmt.setString(1, email);
+            pstmt.setString(2, password);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("id");
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        return found;
+        return -1;
     }
-    public User getUser(int found) {
-        return users.get(found);
+    public User getUser(int id) {
+        String SQL = "SELECT * FROM users WHERE id = ?";
+        try (PreparedStatement pstmt = connection.prepareStatement(SQL)) {
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getString("role").equals("Admin") ? new Admin(rs.getString("name"), rs.getString("email"), rs.getString("password"), rs.getString("role")) : new NormalUser(rs.getString("name"), rs.getString("email"), rs.getString("password"), rs.getString("role"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
-
-    public void AddEvent(Event event){
-        events.add(event);
-        eventNames.add(event.getName());
-    }
-
-
 }
+
+//    public void AddEvent(Event event){
+//        events.add(event);
+//        eventNames.add(event.getName());
+//    }
+//}
