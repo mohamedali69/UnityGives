f35d7ad80651202878a7b7ac05679521b8282c9a
mohamedali69 <dalichakhari6801@gmail.com>
2023-12-18 21:47:21 +0100

    add getUser function to db

--- src/Organisation/Database.java
+++ src/Organisation/Database.java
@@ -3,7 +3,9 @@ package Organisation;
 import com.sun.tools.javac.Main;
 
 import java.awt.print.Book;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 public class Database {
     private ArrayList<User> users = new ArrayList<User>();
@@ -43,7 +45,35 @@ public class Database {
     public User getUser(int found) {
         return users.get(found);
     }
+    private void getUsers() {
+        String text1 = "";
+        try {
+            BufferedReader br1 = new BufferedReader(new FileReader(usersfile));
+            String s1;
+            while ((s1 = br1.readLine()) !=null) {
+                text1 = text1 + s1;
+            }
+            br1.close();
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
 
+        if (!text1.matches("") || !text1.isEmpty()) {
+            String[] a1 = text1.split("<NewUser/>");
+            for (String s : a1) {
+                String[] a2 = s.split("<N/>");
+                if (a2[3].matches("Admin")) {
+                    User user = new Admin(a2[0], a2[1], a2[2], "Admin");
+                    users.add(user);
+                    userNames.add(user.getName());
+                } else {
+                    User user = new NormalUser(a2[0], a2[1], a2[2], "Normal User");
+                    users.add(user);
+                    userNames.add(user.getName());
+                }
+            }
+        }
+    }
     public void AddEvent(Event event){
         events.add(event);
         eventNames.add(event.getName());
